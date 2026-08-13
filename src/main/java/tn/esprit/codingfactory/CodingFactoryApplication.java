ef97187637e84066fcad3146f1e04aa82b7a30e2
khv <hammoudayass@gmail.com>
2026-08-13 13:04:26 +0100

    Initial project setup: Spring Boot + Angular + PostgreSQL config

---
+++ src/main/java/tn/esprit/codingfactory/CodingFactoryApplication.java
@@ -0,0 +1,14 @@
+package tn.esprit.codingfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingFactoryApplication.class, args);
+	}
+
+
+}
